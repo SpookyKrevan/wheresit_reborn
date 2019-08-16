9ccf67a90c31810d2b391526a3b745669a6bdc92
Kyle Evans <6326898+SpookyKrevan@users.noreply.github.com>
2019-08-16 09:47:34 -0600

    New organization schemes

--- wherezit.lua
+++ wherezit.lua
@@ -22,16 +22,17 @@ GameTooltip:HookScript("OnTooltipCleared", OnTooltipCleared)
 
 ChatFrame1:AddMessage("Wherezit Herb/Ore Location Addon Loaded.")
 
--- Vanilla Herbs -- 
-spawnZones[765] = "Elwynn Forest, Tirisfal Glades, Teldrassil, Mulgore, Dun Morogh, Eversong Woods, Azuremyst Isle, Ghostlands, Bloodmyst Isle, Durotar, Gilneas, Loch Modan, Silverpine Forest, Westfall, The Lost Isles, Gilneas City, The Wandering Isle, Darkshore, Northern Barrens, Azshara and Thunder Bluff" -- Silverleaf
-spawnZones[2447] = "Teldrassil, Tirisfal Glades, Durotar, Dun Morogh, Azuremyst Isle, Mulgore, Elwynn Forest, Gilneas, Eversong Woods, The Lost Isles, Ghostlands, Bloodmyst Isle, Loch Modan, Westfall, Gilneas City, Azshara, The Wandering Isle, Darkshore and Northern Barrens" -- Peacebloom
-spawnZones[22710] = "Eversong Woods" -- Bloodthistle
-spawnZones[2449] = "Silverpine Forest, Tirisfal Glades, Elwynn Forest, Redridge Mountains, Teldrassil, Durotar, Mulgore, Dun Morogh, Azuremyst Isle, Eversong Woods, Ghostlands, The Lost Isles, Bloodmyst Isle, The Wandering Isle, Westfall, Gilneas, Wailing Caverns, Azshara, Loch Modan, Gilneas City, Northern Barrens and Darkshore" -- Earthroot
-spawnZones[785] = "Hillsbrad Foothills, Darkshore, Silverpine Forest, Northern Barrens, Azshara, Loch Modan, Redridge Mountains, Westfall, Wetlands, Ghostlands, Duskwood, Stonetalon Mountains, Teldrassil, Bloodmyst Isle, Ashenvale, Wailing Caverns and Southern Barrens" -- Mageroyal
-spawnZones[2452] = "Hillsbrad Foothills, Duskwood, Darkshore, Azshara, Northern Barrens, Wetlands, Redridge Mountains, Westfall, Loch Modan, Silverpine Forest, Stonetalon Mountains, Ghostlands, Bloodmyst Isle, Ashenvale, Razorfen Kraul and Southern Barrens" --Swiftthistle
-spawnZones[2450] = "Hillsbrad Foothills, Duskwood, Darkshore, Azshara, Northern Barrens, Wetlands, Redridge Mountains, Westfall, Loch Modan, Silverpine Forest, Stonetalon Mountains, Ghostlands, Bloodmyst Isle, Ashenvale, Razorfen Kraul and Southern Barrens" --Briarthorn
-spawnZones[3820] = "The Cape of Stranglethorn, Northern Stranglethorn, Tanaris, Northern Barrens, Wetlands, Blasted Lands, Western Plaguelands, Darkshore, Eastern Plaguelands, Redridge Mountains, Swamp of Sorrows, Serpentshrine Cavern, Thousand Needles, Hillsbrad Foothills, Arathi Highlands, Feralas, Silverpine Forest, Ashenvale, Stonetalon Mountains, Desolace, Westfall, Dustwallow Marsh, The Hinterlands, Loch Modan and Moonglade" -- Stranglekelp
-spawnZones[2453] = "Northern Stranglethorn, Wetlands, Redridge Mountains, Ashenvale, Duskwood, Westfall, The Hinterlands, Hillsbrad Foothills, Stonetalon Mountains, Loch Modan, Ghostlands, Arathi Highlands, Bloodmyst Isle, Blackfathom Deeps, Northern Barrens and Wailing Caverns" -- Bruiseweed
+-- Classic Items --
+-- Herbs -- 
+--[[ Silverleaf ]] spawnZones[765] = "Elwynn Forest, Tirisfal Glades, Teldrassil, Mulgore, Dun Morogh, Eversong Woods, Azuremyst Isle, Ghostlands, Bloodmyst Isle, Durotar, Gilneas, Loch Modan, Silverpine Forest, Westfall, The Lost Isles, Gilneas City, The Wandering Isle, Darkshore, Northern Barrens, Azshara and Thunder Bluff"
+--[[ Peacebloom ]] spawnZones[2447] = "Teldrassil, Tirisfal Glades, Durotar, Dun Morogh, Azuremyst Isle, Mulgore, Elwynn Forest, Gilneas, Eversong Woods, The Lost Isles, Ghostlands, Bloodmyst Isle, Loch Modan, Westfall, Gilneas City, Azshara, The Wandering Isle, Darkshore and Northern Barrens"
+--[[ Bloodthistle ]] spawnZones[22710] = "Eversong Woods"
+--[[ Earthroot ]] spawnZones[2449] = "Silverpine Forest, Tirisfal Glades, Elwynn Forest, Redridge Mountains,Teldrassil, Durotar, Mulgore, Dun Morogh, Azuremyst Isle, Eversong Woods, Ghostlands, The Lost Isles, Bloodmyst Isle, The Wandering Isle, Westfall, Gilneas, Wailing Caverns, Azshara, Loch Modan, Gilneas City, Northern Barrens and Darkshore" 
+--[[ Mageroyal ]] spawnZones[785] = "Hillsbrad Foothills, Darkshore, Silverpine Forest, Northern Barrens, Azshara, Loch Modan, Redridge Mountains, Westfall, Wetlands, Ghostlands, Duskwood, Stonetalon Mountains, Teldrassil, Bloodmyst Isle, Ashenvale, Wailing Caverns and Southern Barrens" 
+--[[ Swiftthistle ]] spawnZones[2452] = "Hillsbrad Foothills, Duskwood, Darkshore, Azshara, Northern Barrens, Wetlands, Redridge Mountains, Westfall, Loch Modan, Silverpine Forest, Stonetalon Mountains, Ghostlands, Bloodmyst Isle, Ashenvale, Razorfen Kraul and Southern Barrens" 
+--[[ Briarthorn ]] spawnZones[2450] = "Hillsbrad Foothills, Duskwood, Darkshore, Azshara, Northern Barrens, Wetlands, Redridge Mountains, Westfall, Loch Modan, Silverpine Forest, Stonetalon Mountains, Ghostlands, Bloodmyst Isle, Ashenvale, Razorfen Kraul and Southern Barrens" 
+--[[ Stranglekelp ]] spawnZones[3820] = "The Cape of Stranglethorn, Northern Stranglethorn, Tanaris, Northern Barrens, Wetlands, Blasted Lands, Western Plaguelands, Darkshore, Eastern Plaguelands, Redridge Mountains, Swamp of Sorrows, Serpentshrine Cavern, Thousand Needles, Hillsbrad Foothills, Arathi Highlands, Feralas, Silverpine Forest, Ashenvale, Stonetalon Mountains, Desolace, Westfall, Dustwallow Marsh, The Hinterlands, Loch Modan and Moonglade" 
+--[[ Bruiseweed ]] spawnZones[2453] = "Northern Stranglethorn, Wetlands, Redridge Mountains, Ashenvale, Duskwood, Westfall, The Hinterlands, Hillsbrad Foothills, Stonetalon Mountains, Loch Modan, Ghostlands, Arathi Highlands, Bloodmyst Isle, Blackfathom Deeps, Northern Barrens and Wailing Caverns" 
 spawnZones[3355] = "Northern Stranglethorn, Stonetalon Mountains, Ashenvale, Arathi Highlands, Desolace, Wetlands, Duskwood, The Hinterlands, Stranglethorn Vale and Twilight Highlands" -- Wild Steelbloom
 spawnZones[1628] = "Duskwood, Razorfen Downs, Arathi Highlands, Wetlands and Hillsbrad Foothills" -- Grave Moss
 spawnZones[3356] = "Northern Stranglethorn, Western Plaguelands, Wetlands, Southern Barrens, Desolace, Arathi Highlands, The Hinterlands, Duskwood, Dustwallow Marsh, Feralas, Ashenvale and Wailing Caverns" -- Kingsblood
@@ -53,6 +54,15 @@ spawnZones[13463] = "Blasted Lands, Silithus, Felwood, Hellfire Peninsula, Zanga
 spawnZones[13465] = "Winterspring, Hellfire Peninsula, Un'Goro Crater, Blasted Lands and Silithus" -- Mountain Silversage
 spawnZones[13466] = "Swamp of Sorrows" -- Sorrowmoss
 spawnZones[13467] = "Winterspring" -- Icecap
+-- Ores --
+--[[ Copper ]] spawnZones[2770] = "Elwynn Forest, Dun Morogh, Darkshore, Westfall, Durotar, Tirisfal Glades, Duskwood, Loch Modan, Redridge Mountains, Mulgore, Wetlands, The Barrens, Silverpine Forest, Hillsbrad Foothills, Thousand Needles, Ashenvale and Stonetalon Mountains"
+
+
+
+
+
+
+
 --[[ -- Burning Crusade Herbs --
 spawnZones[176589] = "Blasted Lands, Silithus, Eastern Plaguelands, Winterspring" -- Black Lotus
 spawnZones[22789] = "Terokkar Forest, Shadowmoon Valley, The Botanica" -- Terocone
